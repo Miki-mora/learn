97e3771643d7e479cfb0725e12382c3f6e9481c5
Miki-mora <1011348487@qq.com>
2019-10-12 20:08:02 +0800

    hh

--- src/main/java/edu/hubu/learn/service/MusicService.java
+++ src/main/java/edu/hubu/learn/service/MusicService.java
@@ -1,10 +1,14 @@
 package edu.hubu.learn.service;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import edu.hubu.learn.dao.MusicDao;
 import edu.hubu.learn.entity.Music;
+import java.util.List;
 
 @Service
 public class MusicService {
@@ -15,4 +19,7 @@ public class MusicService {
     public Music getMusic(Long id) {
         return musicDao.findById(id).get();
     }
+    public List<Music> getMusics(){
+        return musicDao.findAll();
+    } 
 }
--- src/main/java/edu/hubu/learn/web/IndexController.java
+++ src/main/java/edu/hubu/learn/web/IndexController.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.hubu.learn.entity.Music;
+
 import edu.hubu.learn.entity.User;
-import edu.hubu.learn.service.MusicService;
+
 import edu.hubu.learn.service.UserService;
 
 @Controller
@@ -17,8 +17,7 @@ public class IndexController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private MusicService musicService;
+
 
     @RequestMapping("/")
     public ModelAndView index() {
@@ -36,13 +35,5 @@ public class IndexController {
         return mav;
     }
 
-    @RequestMapping("/music")
-    public ModelAndView music() {
-        ModelAndView mav = new ModelAndView();
-        Music music = musicService.getMusic(1l);
-        mav.addObject("music", music);
-        mav.setViewName("music");
-        return mav;
-    }
-
+ 
 }
